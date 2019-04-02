7acaab4ec7254960ad9d024a5bdccd61077d4fe6
Jackson Johnson <c3303926@uon.edu.au>
2019-04-02 02:52:24 +0000

    master:

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	printf("Woo! You got the template working!\n");
-	return 0;
+int main()
+{
+    int q;
+    char letters [26] {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}; //{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
+    {
+        printf("Insert sentence: "); //Input a s word or sentence
+        scanf(letters[], &c); //Read the sentence to be encoded
+        c = c + 3 // implements new code
+        printf("New sentence: ")
+        for (q = 0; q < 26; q++); //( q=0 ; q<=255 ; q++ )
+    }
 }
+
